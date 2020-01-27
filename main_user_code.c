df110150a9cbcec0adc920fc4c4061bfda5d31db
Rafael Pastre <44172578+rafael-pastre@users.noreply.github.com>
2020-01-27 16:14:51 -0300

    Update main_user_code.c

--- main_user_code.c
+++ main_user_code.c
@@ -1,3 +1,4 @@
+  //TX Config
   HAL_StatusTypeDef status = HAL_OK;
   rfmTX = (RFM96*)malloc(sizeof(RFM96));
   status = RFM96_set_pins(rfmTX, hspi2, SS_COMM_TX_GPIO_Port, SS_COMM_TX_Pin, RESET_COMM_TX_GPIO_Port, RESET_COMM_TX_Pin);
@@ -6,8 +7,25 @@
   status = RFM96_set_signal_bandwidth(rfmTX, RFM96_LORA_BW_125);
   status = RFM96_set_spreading_factor(rfmTX, 11);
   status = RFM96_enable_crc(rfmTX);
+
+  //RX Config
+  rfmRX = (RFM96*)malloc(sizeof(RFM96));
+  status = RFM96_set_pins(rfmRX, hspi2, SS_COMM_RX_GPIO_Port, SS_COMM_RX_Pin, RESET_COMM_RX_GPIO_Port, RESET_COMM_RX_Pin);
+  status = RFM96_init(rfmRX, 433.123E6, 0x12);
+  status = RFM96_set_signal_bandwidth(rfmRX, RFM96_LORA_BW_125);
+  status = RFM96_set_spreading_factor(rfmRX, 11);
+  status = RFM96_enable_crc(rfmRX);
+  status = RFM96_explicit_header_mode(rfmRX);
+  status = RFM96_set_op_mode(rfmRX, RFM96_LORA_MODE_RX_CONT);
+
   //Teste LoRa com funcoes proprias
   uint8_t teste[32] = "Teste";
-  status = RFM96_send_packet(rfmTX, teste, sizeof(teste));	//loraTX.sendPacket(teste, sizeof(teste));
-  loraRX.readPacket(RxData, sizeof(RxData));
-  teste[0] = 0;
+  uint8_t RxData[128];
+  status = RFM96_send_packet(rfmTX, teste, sizeof(teste));
+  status = RFM96_read_packet(rfmRX, RxData, sizeof(RxData));
+  
+  while(1){
+	  teste[0]++;
+	  status = RFM96_send_packet(rfmTX, teste, sizeof(teste));
+	  status = RFM96_read_packet(rfmRX, RxData, sizeof(RxData));//loraRX.readPacket(RxData, sizeof(RxData));
+  }
